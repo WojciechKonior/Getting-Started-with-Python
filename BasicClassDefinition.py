b0075bcb2ca59bcb2a1771dcc50f6be4f1e95934
VoyTec <78218606+WojciechKonior@users.noreply.github.com>
2023-08-24 20:20:53 +0200

    Update BasicClassDefinition.py

--- BasicClassDefinition.py
+++ BasicClassDefinition.py
@@ -15,8 +15,13 @@ class Book:
   def setdiscount(self, amount):
     self._discount = amount
 
+class Newspaper:
+  def __init__(self, name):
+    self.name = name
+
 b1 = Book("Trylogia", "Siekniewicz", 1000, 40.99)
 b2 = Book("Quo Vadis", "Sienkiewicz", 400, 50.99)
+n1 = Newspaper("Newsweek")
 
 print(b1)
 print(b1.title)
@@ -24,3 +29,13 @@ print(b1.getprice())
 b1.setdiscount(10)
 print(b1.getprice())
 print(b2._Book__secret)
+
+print(type(b1))
+print(type(n1))
+print(type(b1)==type(b2))
+print(type(b1)==type(n1))
+
+print(isinstance(b1, Book))
+print(isinstance(n1, Newspaper))
+print(isinstance(n1, Book))
+print(isinstance(n1, object))
