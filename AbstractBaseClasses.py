f56d12c945c35b3f164bb259ef3ad2b49e975bfd
VoyTec <78218606+WojciechKonior@users.noreply.github.com>
2023-08-25 14:00:50 +0200

    Update AbstractBaseClasses.py

--- AbstractBaseClasses.py
+++ AbstractBaseClasses.py
@@ -1,1 +1,31 @@
+from abc import ABC
 
+
+class GraphicShape(ABC):
+    def __init__(self):
+        super().__init__()
+
+    def calcArea(self):
+        pass
+
+
+class Circle(GraphicShape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def calcArea(self):
+        return 3.14*self.radius*self.radius
+
+
+class Square(GraphicShape):
+    def __init__(self, side):
+        self.side = side
+
+    def calcArea(self):
+        return self.side*self.side
+
+
+c = Circle(10)
+s = Square(12)
+
+print(c.calcArea(), s.calcArea())
