8cca10de762a7f1c52572549036010bb1606525a
VoyTec <78218606+WojciechKonior@users.noreply.github.com>
2023-08-26 11:38:34 +0200

    Update MagicMethods.py

--- MagicMethods.py
+++ MagicMethods.py
@@ -23,12 +23,36 @@ class Book:
             raise ValueError(f"cannot compare {type(self)} with {type(book)}")
         return self.price > book.price
 
+    def __ge__(self, book):
+        if not isinstance(book, Book):
+            raise ValueError(f"cannot compare {type(self)} with {type(book)}")
+        return self.price >= book.price
+
+    def __lt__(self, book):
+        if not isinstance(book, Book):
+            raise ValueError(f"cannot compare {type(self)} with {type(book)}")
+        return self.price < book.price
+
+    def __le__(self, book):
+        if not isinstance(book, Book):
+            raise ValueError(f"cannot compare {type(self)} with {type(book)}")
+        return self.price <= book.price
+
 
 b1 = Book("Quo Vadis", "Sienkiewicz", 40.3)
-b2 = Book("Faraon", "Prus", 40.3)
+b2 = Book("Faraon", "Prus", 4.3)
+b3 = Book("Quo Vadis", "Sienkiewicz", 43.3)
+b4 = Book("Faraon", "Prus", 45.3)
+b5 = Book("Quo Vadis", "Sienkiewicz", 48.3)
+b6 = Book("Faraon", "Prus", 30.3)
 
 print(str(b1))
 print(repr(b1))
+
+books = [b1, b2, b3, b4, b5, b6]
+books.sort()
+
+print([book.title for book in books])
 
 print(b1==b2)
 print(b1==40)
