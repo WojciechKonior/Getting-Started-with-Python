824ab2d5c9d42f161ef023cd54b1f95c8f7c6093
Wojciech Konior <konior.wojciech@gmail.com>
2023-09-21 20:17:55 +0200

    some refactoring

--- LinkedInTrainings/NetworkProgrammingInC/gethostdetails.c
+++ LinkedInTrainings/NetworkProgrammingInC/gethostdetails.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <netdb.h>
+#include <arpa/inet.h>
 
 int main()
 {
     char host[BUFSIZ];
     int r;
+    struct hostent *hostdata;
+    char** addresses;
     r = gethostname(host, BUFSIZ);
     if(r==-1)
     {
@@ -13,5 +17,19 @@ int main()
         exit(1);
     }
     printf("The host is %s\n", host);
+
+    hostdata = gethostbyname(host);
+    if(hostdata == NULL)
+    {
+        fprintf(stderr, "Can't obtain hostdata\n");
+        exit(1);
+    }
+    printf("Addresses: \n");
+    addresses = hostdata->h_addr_list;
+    while(*addresses)
+    {
+        printf("%s\n", inet_ntoa(*(struct in_addr *)*addresses));
+        addresses++;
+    }
     return 0;
 }
