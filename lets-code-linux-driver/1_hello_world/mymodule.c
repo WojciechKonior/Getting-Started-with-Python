ccdeb7ec4882f642dc5c6f0886eafbf5cfc120dc
Wojciech Konior <konior.wojciech@gmail.com>
2024-05-16 23:25:32 +0200

    kernel module added

--- lets-code-linux-driver/1_hello_world/mymodule.c
+++ lets-code-linux-driver/1_hello_world/mymodule.c
@@ -1,17 +1,24 @@
-#include <linux/init.h>
 #include <linux/module.h>
+#include <linux/sched.h>
 
-// Meta infrormation
-MODULE_LICENCE("GPL");
-MODULE_AUTHOR("wkonior");
-MODULE_DESCRIPTION("Hello world module");
+MODULE_LICENSE("GPL");
 
-static int __init ModuleInit(void) {
-  printk("Hello Kernel\n");
-  return 0;
+static int errval = 0;
+
+module_param(errval, int, S_IRWXU);
+MODULE_PARM_DESC(errval, "Value to return from init function");
+
+int my_init_func(void) {
+  printk(KERN_CRIT "CRIT: errval=%d\n", errval);
+  printk(KERN_ERR "ERR: errval=%d\n", errval);
+  printk(KERN_INFO "INFO: errval=%d\n", errval);
+  printk(KERN_DEBUG "DBG: errval=%d\n", errval);
+  return errval;
 }
 
-static void __exit ModuleExit(void) { printk("Goodbye Kernel\n"); }
+void my_cleanup_func(void) { 
+    printk(KERN_WARNING "The module is now unloaded\n"); 
+}
 
-module_init(ModuleInit);
-module_exit(ModuleExit);
+module_init(my_init_func);
+module_exit(my_cleanup_func);
