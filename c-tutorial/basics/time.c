ea831f94f3f2f4955fc0c1c8a3537dc181925e8a
Wojciech Konior <konior.wojciech@gmail.com>
2024-05-27 18:26:36 +0200

    some c time library

---
+++ c-tutorial/basics/time.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+
+int main(int argc, char*argv[])
+{
+
+    time_t now = time(NULL);
+    printf("Epoch: %ld\n", now);
+    sleep(2);
+
+    time_t two_secs = time(NULL);
+    printf("Epoch: %d\n", two_secs);
+
+    double diff = difftime(two_secs,now);
+    printf("Diff: %lf\n", diff);
+
+    char *str_now = ctime(&now);
+    printf("%s\n", str_now);
+
+    struct tm *gm_time = gmtime(&now);
+    printf("tm_sec: %d\n", gm_time->tm_sec);
+
+    struct tm *cur_time =localtime(&now);
+    char *other_string = asctime(cur_time);
+    printf("%s\n", other_string);
+
+    time_t curr_time = mktime(cur_time);
+
+    char s[100];
+    strftime(s,100, "%A %B %d", cur_time);
+
+    clock_t start, end;
+    start = clock();
+    for(int i = 0; i<1e6; i++){}
+    end = clock();
+
+    double total = (double)(end-start)/CLOCKS_PER_SEC;
+    printf("Time: %fs\n", total);
+
+    printf("\nDone.\n");
+    return 0;
+}
