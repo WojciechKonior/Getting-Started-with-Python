4b9cd91699e588f9b7d3a2e0b07c1e13c7464d64
Wojciech Konior <konior.wojciech@gmail.com>
2024-10-03 13:32:36 +0200

    Update flow-control.cpp

--- cpp-tutorial/cpp-essential-training/flow-control.cpp
+++ cpp-tutorial/cpp-essential-training/flow-control.cpp
@@ -1,1 +1,23 @@
+#include <format>
+#include <iostream>
 
+using std::format;
+using std::cout;
+
+int main(){
+  auto x = 42;
+  auto y = 7;
+  if(x>y){
+    cout << "condition is true\n";
+  }
+  else if(x<=y){
+    cout << "condition is true\n";
+  }
+  else if(x!=y){
+    cout << "condition is true\n";
+  }
+  else{
+    cout << "all conditions are false\n";
+  }
+  cout << "after\n";
+}
