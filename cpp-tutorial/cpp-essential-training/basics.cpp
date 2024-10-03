4cb85f6dae09b14355bbddadaf5290c96405d727
Wojciech Konior <konior.wojciech@gmail.com>
2024-10-03 13:10:16 +0200

    Update basics.cpp

--- cpp-tutorial/cpp-essential-training/basics.cpp
+++ cpp-tutorial/cpp-essential-training/basics.cpp
@@ -6,7 +6,13 @@ using std::format;
 
 int main(){
   int x;
+  auto k = 47;
+  const int i{}; //initialized with 0
+  int j{1}; //initialized with 1
   x = 42;
+  int* ip;
+  ip = &x;
+  int y = *ip;
   auto str = format("x is {}\n", x);
   cout << str;
 }
